36aa425ae90c011ab7d829c636586f7cef817c29
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-07-31 23:58:25 +0900

    Feat: 추가. 콘솔 출력문 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,24 +3,46 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
+
 public class Application {
 
     private final int NUMBER_MAX = 3;
 
     public static void main(String[] args) {
         Application application = new Application();
-        application.getUserNum();
     }
 
-    public int makeRandomNum(){
-        return Randoms.pickNumberInRange(1,9);
+    public void playGame(){
+        int[] randomNumbers = makeThreeRanNum();
+        while (true){
+            boolean outCheck = playOneCycle(randomNumbers);
+            // playOneCycle 에서 boolean 을 리턴하는 것이 아니라, 볼카운트를 리턴하도록
+            if(outCheck){
+
+            }
+        }
     }
 
-    public int[] makeThreeRanNum(){
+    public boolean playOneCycle(int[] randomNumbers) {
+        int[] userNums = getUserNum();
+        int[] ballCounts = makeBallCount(randomNumbers, userNums);
+        String string = Arrays.toString(ballCounts);
+        System.out.println(Arrays.toString(randomNumbers));
+        System.out.println(string);
+        viewResult(randomNumbers);
+        return checkOut(userNums[0]);
+    }
+
+    public int makeRandomNum() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+
+    public int[] makeThreeRanNum() {
         int[] threeNum = new int[NUMBER_MAX];
-        for(int i =0; i< threeNum.length;){
+        for (int i = 0; i < threeNum.length; ) {
             int randomNum = makeRandomNum();
-            if(checkNotContain(threeNum, randomNum)){
+            if (checkContainNum(threeNum, randomNum)) {
                 threeNum[i] = randomNum;
                 i++;
             }
@@ -28,35 +50,37 @@ public class Application {
         return threeNum;
     }
 
-    public boolean checkNotContain(int[] numbers, int randomNum){
-        for( int i =0; i< numbers.length; i++){
-            if(numbers[i] == randomNum){
+    public boolean checkContainNum(int[] numbers, int randomNum) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == randomNum) {
                 return false;
             }
         }
         return true;
     }
 
-    public int[] getUserNum(){
+    public int[] getUserNum() {
         int[] userNums = new int[NUMBER_MAX];
-        for(int i =0; i<NUMBER_MAX; i++){
-            String input = Console.readLine();
-            try {
-                int inputNum = Integer.parseInt(input);
-                userNums[i] = inputNum;
-            }catch (NumberFormatException e){
-                throw new IllegalArgumentException("잘못된 입력값입니다. 게임을 종료합니다.");
+        System.out.print("숫자를 입력해 주세요 : ");
+        String input = Console.readLine();
+        String[] splited = input.split("");
+        try {
+            for (int i = 0; i < userNums.length; i++) {
+                int parsedInt = Integer.parseInt(splited[i]);
+                userNums[i] = parsedInt;
             }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력값입니다. 게임을 종료합니다.");
         }
         return userNums;
     }
 
-    public int[] makeBallCount(int[] randomNums, int[] userNums){
+    public int[] makeBallCount(int[] randomNums, int[] userNums) {
         int[] ballCount = new int[2];
-        for(int i =0; i< randomNums.length; i++){
+        for (int i = 0; i < randomNums.length; i++) {
             int strikeCheck = strikeCheck(randomNums[i], userNums[i]);
             ballCount[0] += strikeCheck;
-            if(strikeCheck == 1){
+            if (strikeCheck != 1) {
                 ballCount[1] += ballCheck(randomNums[i], userNums);
             }
         }
@@ -64,21 +88,55 @@ public class Application {
     }
 
     public int strikeCheck(int checkNum, int userNum) {
-        if(checkNum == userNum){
+        if (checkNum == userNum) {
             return 1;
         }
         return 0;
     }
 
     public int ballCheck(int checkNum, int[] userNums) {
-        for(int i=0; i<userNums.length; i++){
-            if(userNums[i] == checkNum){
+        for (int i = 0; i < userNums.length; i++) {
+            if (userNums[i] == checkNum) {
                 return 1;
             }
         }
         return 0;
     }
 
-    // 스트라이크를 체크하고, 스트라이크가 아닌 숫자에 대해서만 볼 체크도 하는게 더 나은듯
+    public boolean checkOut(int strike) {
+        if (strike == 3) {
+            return true;
+        }
+        return false;
+    }
 
+    public void viewStrike(int strikeCount) {
+        if (strikeCount > 0) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+    }
+
+    public void viewBall(int ballCount) {
+        if (ballCount > 0) {
+            System.out.println(ballCount + "볼");
+        }
+    }
+
+    public void mergeViews(int[] ballcount){
+        viewBall(ballcount[1]);
+        viewStrike(ballcount[0]);
+    }
+
+    public void viewOut() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void viewResult(int[] ballCount) {
+        boolean outCheck = checkOut(ballCount[0]);
+        if (outCheck) {
+            viewOut();
+        }
+        mergeViews(ballCount);
+    }
 }
